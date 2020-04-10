ce779abcc4ca80bed6273a2e98cfe0af96e49844
Paul Beaudet <inof8or@gmail.com>
2020-04-10 15:48:06 -0400

    make sure are photos and rename them to day time based on exif taken time

--- upload.go
+++ upload.go
@@ -3,18 +3,15 @@ package main
 import (
     "fmt"
     "os"
-    "time"
-    "strconv"
+    "strings"
+
+    "github.com/rwcarlsen/goexif/exif"
 )
 
 func main(){
     const uploadDefaultFolder = "/home/wooper/Pictures/test/uploads/"
     photosFolderDefualt := "/home/wooper/Pictures/test/personal_pictures/"
-    // testMove(uploadDefaultFolder, photosFolderDefualt)
-    ls(uploadDefaultFolder)
     scanAndMove(uploadDefaultFolder, photosFolderDefualt)
-    ls(uploadDefaultFolder)
-    ls(photosFolderDefualt)
 }
 
 func scanAndMove(uploadDir string, photoDir string){
@@ -23,19 +20,68 @@ func scanAndMove(uploadDir string, photoDir string){
     files, error := uploads.Readdir(-1)
     uploads.Close()
     if error != nil{panic(error)}
-    fmt.Println("scanning contents of " + uploadDir)
     for _, file := range files {
-        move(file, uploadDir, photoDir)
+        if ext := isPhotoExtention(file.Name()); ext != ""{
+            moveAndRename(file, uploadDir, photoDir, ext)
+        } else {
+            fmt.Println(file.Name() + " is not a photo");
+        }
     }
 }
 
-func move(file os.FileInfo, sourceDir string, destDir string){
+func moveAndRename(file os.FileInfo, sourceDir string, destDir string, ext string){
     currentLocation := sourceDir + file.Name()
-    monthDay, year := getCreation(currentLocation)
-    fmt.Println(file.Name() + " was created in " + year + " on " + monthDay);
+    monthDay, year, hourMinutes := timeTaken(currentLocation)
     nextDest := destDir + year + "/" + monthDay
     mkdir(nextDest)
-    moveFile(currentLocation, nextDest + "/" + file.Name())
+    moveFile(currentLocation, nextDest + "/" + hourMinutes + ext)
+}
+
+func isPhotoExtention(fileName string)(ext string){
+    fileName = strings.ToLower(fileName)
+    if strings.HasSuffix(fileName, ".rw2"){
+        return ".rw2"
+    } else if strings.HasSuffix(fileName, ".jpg"){
+        return ".jpg"
+    } else {
+        return ""
+    }
+}
+
+func timeTaken(photoPath string)(mmdd string, yyyy string, hhmm string){
+    file, err := os.Open(photoPath)
+    if err != nil {panic(err)}
+    exifData, xerr := exif.Decode(file)
+    if xerr != nil {panic(xerr)}
+    taken, terr := exifData.DateTime()
+    if terr != nil {panic(terr)}
+    monthDay := taken.Format("01_02_")
+    year := taken.Format("2006")
+    hourMinutes := taken.Format("15_04_05")
+    fmt.Println(photoPath + "\n ---> Taken on month and day: " + monthDay +
+        " in year " + year + " at exactly " + hourMinutes)
+    return monthDay, year, hourMinutes
+}
+
+func getCreation(pathToFile string)(mmdd string, yyyy string, hhmm string){
+    fileStat, err := os.Stat(pathToFile)
+    if err != nil {panic(err)}
+    timeObj := fileStat.ModTime()
+    monthDay := timeObj.Format("01_02_")
+    year := timeObj.Format("2006")
+    hourMinutes := timeObj.Format("15_04_05")
+    return monthDay, year, hourMinutes
+}
+
+func mkdir(dirToCreate string){
+    _, error := os.Stat(dirToCreate)
+    if os.IsNotExist(error){
+        if err := os.MkdirAll(dirToCreate, 0755); err != nil {panic(err)}
+    }
+}
+
+func moveFile(oldPath string, newPath string){
+    if err := os.Rename(oldPath, newPath); err != nil{panic(err)}
 }
 
 func ls(dir string){
@@ -48,71 +94,4 @@ func ls(dir string){
     for _, file := range files {
         fmt.Println(file.Name())
     }
-}
-
-func getCreation(pathToFile string)(creationDay string, creationYear string){
-    fileStat, err := os.Stat(pathToFile)
-    if err != nil {
-        panic(err)
-    }
-    timeObj := fileStat.ModTime()
-    monthDay := timeObj.Format("01_02_")
-    year := timeObj.Format("2006")
-    return monthDay, year
-}
-
-func mkdir(dirToCreate string){
-    // dirToCreate := path + nameOfDir
-    _, error := os.Stat(dirToCreate)
-
-    if os.IsNotExist(error){
-        if err := os.MkdirAll(dirToCreate, 0755); err != nil {
-            panic(err);
-        }
-    }
-}
-
-func moveFile(oldPath string, newPath string){
-    if err := os.Rename(oldPath, newPath); err != nil{
-        panic(err)
-    }
-    fmt.Println("reorganizing " + oldPath + " to " + newPath)
-}
-
-func writeFile(fileName string, fileString string){
-    file, err := os.Create(fileName)
-    if err != nil {
-        panic(err)
-    }
-    file.WriteString(fileString + "\n")
-    file.Close()
-}
-
-func readFile(fileName string){
-    file, err := os.Open(fileName)
-    if err != nil{
-        panic(err)
-    }
-    data := make([]byte, 30)
-    file.Read(data)
-    fmt.Printf(string(data) + "\n")
-    file.Close()
-}
-
-
-func testMove(uploadDefaultFolder string, photosFolderDefualt string){
-    // const wd = "/home/wooper/Dropbox/programs/golang/upload_organizer/"
-    year, _, _ := time.Now().Date()
-    photosFolderDefualt = photosFolderDefualt + strconv.Itoa(year) + "/"
-    const testFile = "testFile1.txt"
-    fileDest := uploadDefaultFolder + testFile
-    writeFile(fileDest, "this is a test of the erm");
-    ls(uploadDefaultFolder)
-    modTime, _ := getCreation(fileDest)
-    mkdir(photosFolderDefualt + modTime);
-    newFolderForFile := photosFolderDefualt + modTime + "/"
-    moveFile(fileDest, newFolderForFile + testFile);
-    ls(newFolderForFile)
-    ls(uploadDefaultFolder)
-    readFile(newFolderForFile + testFile);
 }
